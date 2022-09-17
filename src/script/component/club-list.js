715ea6f45ac2f63310467901c3b864c3af055f2a
kisnak21 <krisnastya21@gmail.com>
2022-09-17 10:38:04 +0700

    implementasi AJAX menggunakan fetch

--- src/script/component/club-list.js
+++ src/script/component/club-list.js
@@ -1,22 +1,33 @@
 import './club-item.js';
- 
+
 class ClubList extends HTMLElement {
+
   constructor() {
     super();
     this.shadowDOM = this.attachShadow({mode: 'open'});
   }
- 
+
   set clubs(clubs) {
     this._clubs = clubs;
     this.render();
   }
- 
+
+  render() {
+    this.shadowDOM.innerHTML = '';
+
+    this._clubs.forEach(club => {
+      const clubItemElement = document.createElement('club-item');
+      clubItemElement.club = club;
+      this.shadowDOM.appendChild(clubItemElement);
+    });
+  }
+
   renderError(message) {
     this.shadowDOM.innerHTML = `
       <style>
         .placeholder {
           font-weight: lighter;
-          color: rgba(0,0,0,0.5);
+          color: rgba(0, 0, 0, 0.5);
           -webkit-user-select: none;
           -moz-user-select: none;
           -ms-user-select: none;
@@ -24,17 +35,9 @@ class ClubList extends HTMLElement {
         }
       </style>
     `;
+
     this.shadowDOM.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   }
- 
-  render() {
-    this.shadowDOM.innerHTML = '' ;
-    this._clubs.forEach(club => {
-      const clubItemElement = document.createElement('club-item');
-      clubItemElement.club = club;
-      this.shadowDOM.appendChild(clubItemElement);
-    });
-  }
 }
- 
+
 customElements.define('club-list', ClubList);
--- src/script/component/search-bar.js
+++ src/script/component/search-bar.js
@@ -1,25 +1,27 @@
 class SearchBar extends HTMLElement {
 
-    constructor(){
-      super();
-      this.shadowDOM = this.attachShadow({mode: 'open'})
-    }
-    connectedCallback() {
-        this.render();
-    }
+  constructor() {
+    super();
+    this.shadowDOM = this.attachShadow({mode: 'open'});
+  }
 
-    set clickEvent(event) {
-        this._clickEvent = event;
-        this.render();
+  connectedCallback() {
+    this.render();
+  }
 
-    }
+  set clickEvent(event) {
+    this._clickEvent = event;
+    this.render();
+  }
 
-    get value(){
-      return this.shadowDOM.querySelector('#searchButtonElement').value;
-    }
-    render() {
-        this.shadowDOM.innerHTML = `
-        <style>
+
+  get value() {
+    return this.shadowDOM.querySelector('#searchElement').value;
+  }
+
+  render() {
+    this.shadowDOM.innerHTML = `
+      <style>
         .search-container {
           max-width: 800px;
           box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
@@ -30,7 +32,6 @@ class SearchBar extends HTMLElement {
           top: 10px;
           background-color: white;
         }
-        
         .search-container > input {
           width: 75%;
           padding: 16px;
@@ -38,21 +39,17 @@ class SearchBar extends HTMLElement {
           border-bottom: 1px solid cornflowerblue;
           font-weight: bold;
         }
-        
         .search-container > input:focus {
           outline: 0;
           border-bottom: 2px solid cornflowerblue;
         }
-        
         .search-container > input:focus::placeholder {
           font-weight: bold;
         }
-        
-        .search-container > input::placeholder {
+        .search-container >  input::placeholder {
           color: cornflowerblue;
           font-weight: normal;
         }
-        
         .search-container > button {
           width: 23%;
           cursor: pointer;
@@ -63,33 +60,30 @@ class SearchBar extends HTMLElement {
           border: 0;
           text-transform: uppercase;
         }
-        
         @media screen and (max-width: 550px) {
           .search-container {
             flex-direction: column;
             position: static;
           }
-        
           .search-container > input {
             width: 100%;
             margin-bottom: 12px;
           }
-        
           .search-container > button {
             width: 100%;
           }
         }
-        </style>
-        <div id="search-container" class="search-container">
-          <input placeholder="Search football club" id="searchElement" type="search">
-          <button id="searchButtonElement" type="submit">Search</button>
-        </div>
-      `;
+      </style>
+      
+      <div id="search-container" class="search-container">
+        <input placeholder="Search football club" id="searchElement" type="search">
+        <button id="searchButtonElement" type="submit">Search</button>
+      </div>
+    `;
 
-        this
-            .shadowDOM.querySelector('#searchButtonElement')
-            .addEventListener('click', this._clickEvent);
-    }
+    this.shadowDOM.querySelector('#searchButtonElement')
+        .addEventListener('click', this._clickEvent);
+  }
 }
 
 customElements.define('search-bar', SearchBar);
